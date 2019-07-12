52245909c15faef3fb6ed8f466de525f82c5d319
GH-Lim <gunhyuck11@gmail.com>
2019-07-12 16:41:34 +0900

    Implement Translation functionality using Naver Papago

--- app.py
+++ app.py
@@ -4,7 +4,13 @@ import pprint
 import requests # 요청
 
 app = Flask(__name__)
+
+# Telegram info
 API_TOKEN = config('API_TOKEN') # 상수는 대문자
+# Naver info
+NAVER_CLIENT_ID = config('NAVER_CLIENT_ID')
+NAVER_CLIENT_SECRET = config('NAVER_CLIENT_SECRET')
+
 base_url = 'https://api.telegram.org'
 
 @app.route('/')
@@ -27,8 +33,35 @@ def telegram():
         chat_id = from_telegram.get('message').get('chat').get('id')    # 보낸 사람의 아이디
         text = from_telegram.get('message').get('text')                 # 보낸 메세지
 
-        if text == '안녕':
-            text = '오셨습니까'
+        # 첫 네글자가 '/번역 ' 일 때
+        if text[0:4] == '/한영 ':   # 텍스트도 index 로 접근이 가능하다.
+            headers = {
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,   # , 있는걸 추천 컨벤션 추가할때 혹시라도 오류가 생길까봐
+            }                                                   # trailing comma
+            data = {
+                'source': 'ko',
+                'target': 'en',
+                'text': text[4:],   # '/번역 ' 이후의 문자열만 대상으로 번역
+            }
+            papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+            papago_res = requests.post(papago_url, headers=headers, data=data)
+            # pprint.pprint(papago_res.json())
+            text = papago_res.json().get('message').get('result').get('translatedText')
+        if text[0:4] == '/영한 ':   # 텍스트도 index 로 접근이 가능하다.
+            headers = {
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,
+            }
+            data = {
+                'source': 'en',
+                'target': 'ko',
+                'text': text[4:],   # '/번역 ' 이후의 문자열만 대상으로 번역
+            }
+            papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+            papago_res = requests.post(papago_url, headers=headers, data=data)
+            # pprint.pprint(papago_res.json())
+            text = papago_res.json().get('message').get('result').get('translatedText')
 
         api_url = f'{base_url}/bot{API_TOKEN}/sendMessage?chat_id={chat_id}&text={text}'
         requests.get(api_url)
